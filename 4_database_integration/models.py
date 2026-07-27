d4e3c900157ef42fa2250d7751928b4a3c22207e
MuazulHasanKhan <muazulhasankhan@gmail.com>
2026-07-27 19:49:33 +0530

    ..

--- 4_database_integration/models.py
+++ 4_database_integration/models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 class Employee(Base):
     __tablename__ = 'employees'
-    id = column(Integer, primary_key=True, index=True)
-    name = column(String(50), nullable = False)
-    email = column(String(50), unique = True, nullable = False)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable = False)
+    email = Column(String(50), unique = True, nullable = False)
 
 
 

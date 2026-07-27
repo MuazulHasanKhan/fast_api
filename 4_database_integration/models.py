69cdde18b45638e027a4d941f2ca3bf4bb5d601f
MuazulHasanKhan <muazulhasankhan@gmail.com>
2026-07-27 16:32:56 +0530

    ..

--- 4_database_integration/models.py
+++ 4_database_integration/models.py
@@ -1,2 +1,12 @@
-from pydantic import BaseModel, Field
+from sqlalchemy import column, Integer, String, Float
+from database import Base
 
+class Employee(Base):
+    __tablename__ = 'employees'
+    id = column(Integer, primary_key=True, index=True)
+    name = column(String(50), nullable = False)
+    email = column(String(50), unique = True, nullable = False)
+
+
+
+    

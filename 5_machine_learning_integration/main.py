51e40846e80b9574f726ed23f64943dcdc4201b5
MuazulHasanKhan <muazulhasankhan@gmail.com>
2026-07-30 00:07:28 +0530

    batch prediction

--- 5_machine_learning_integration/main.py
+++ 5_machine_learning_integration/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from schemas import InputSchema, OutputSchema
-from predict import make_prediction
+from predict import make_prediction, batch_predict
 
 app = FastAPI()
 
@@ -13,3 +13,9 @@ def index():
 def predict(input_data: InputSchema):
     prediction = make_prediction(input_data.model_dump()) # model_dump() is used to convert json to dict
     return {"price": round(prediction, 2)}
+
+
+@app.post('/batchpredict', response_model=list[OutputSchema])
+def batch_predict(user_inputs: list[InputSchema]):
+    predictions = batch_predict([user_input.model_dump() for user_input in user_inputs])
+    return [OutputSchema(price=round(prediction, 2)) for prediction in predictions]
--- 5_machine_learning_integration/predict.py
+++ 5_machine_learning_integration/predict.py
@@ -9,4 +9,7 @@ def make_prediction(data:dict)-> float:
     # model expects numpy object, so we need to convert the input data to a numpy array
     input_data = np.array(list(data.values())).reshape(1, -1)
     return model.predict(input_data)[0]
-    
+
+def batch_predict(data: list[dict])-> np.array:
+    input_data = np.array([list(d.values()) for d in data])
+    return model.predict(input_data)
